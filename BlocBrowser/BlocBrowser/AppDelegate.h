4614d9d6d616d2e3c9ae5c90282ffe574fe8a04f
Bonnie <aalle.allen@gmail.com>
2016-03-03 18:28:11 -0500

    Set the root view controller in appdelegate and created a UIWindow set the View Controller as the rootViewController of the navigation controller and created the Main container View to place all subviews

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by King Kittenhead on 3/3/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BlocBrowser
+//
+//  Created by King Kittenhead on 3/3/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
